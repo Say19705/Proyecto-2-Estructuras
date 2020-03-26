5fe0ce45cdd50bab05437797fd84db233d31fd48
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-03-25 18:27:17 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) throws Exception{
+		System.out.println("Aqui vamos a comenzar el proyecto y debemos comenzar ya");
+	}
+
+}
